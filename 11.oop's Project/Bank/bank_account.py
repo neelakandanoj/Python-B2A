2a50b79ee935cd761781f3e146a7e8511e9a0051
neelakandan <neelakandan2001kpm@gmail.com>
2024-07-15 21:58:21 +0530

    OOps

--- 11.oop's Project/Bank/bank_account.py
+++ 11.oop's Project/Bank/bank_account.py
@@ -44,8 +44,8 @@ class BankAccount:
         except BalanceException as error:
             print(f"Transfer Interrupted.")            
 
-
-
+ 
+ 
 class InterestRewardsAcct(BankAccount):
     def deposit(self, amount):
         self.balance=self.balance+(amount * 1.05)
